31053f7db37d134b8c58eb3ad97dc35e70ad5e8d
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2025-04-17 00:23:34 +0200

    Fix generator

--- appsflyer/src/androidMain/kotlin/com/metacto/kmm/appsflyer/ShareLinkGenerator.kt
+++ appsflyer/src/androidMain/kotlin/com/metacto/kmm/appsflyer/ShareLinkGenerator.kt
@@ -1,9 +1,14 @@
+@file:OptIn(InternalCoroutinesApi::class)
+
 package com.metacto.kmm.appsflyer
 
+import com.appsflyer.share.LinkGenerator
 import com.appsflyer.share.ShareInviteHelper
 import com.metacto.kmm.appsflyer.model.BaseUrl
 import com.metacto.kmm.appsflyer.util.AppsFlyerConstants
 import com.metacto.kmm.appsflyer.util.generateDeepLinkValue
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.suspendCancellableCoroutine
 
 actual object ShareLinkGenerator {
     actual suspend fun generateShareLink(
@@ -22,71 +27,83 @@ actual object ShareLinkGenerator {
         customShortLink: String?,
         parameters: Map<String, String>?
     ): String? {
-        if (context == null || context !is android.content.Context) {
-            throw IllegalArgumentException("Context is required and must be an instance of android.content.Context")
-        }
-        val generator = ShareInviteHelper.generateInviteUrl(context)
+        return suspendCancellableCoroutine { cont ->
+            if (context == null || context !is android.content.Context) {
+                throw IllegalArgumentException("Context is required and must be an instance of android.content.Context")
+            }
+            val generator = ShareInviteHelper.generateInviteUrl(context)
 
-        val deepLinkValue = generateDeepLinkValue(
-            destination,
-            channel,
-            referrerCustomerId,
-            baseDeepLink,
-            referrerName,
-            referrerUID,
-            campaign,
-            referrerImageURL,
-            parameters
-        )
+            val deepLinkValue = generateDeepLinkValue(
+                destination,
+                channel,
+                referrerCustomerId,
+                baseDeepLink,
+                referrerName,
+                referrerUID,
+                campaign,
+                referrerImageURL,
+                parameters
+            )
 
-        generator.addParameter(AppsFlyerConstants.DEEP_LINK_VALUE, deepLinkValue)
+            generator.addParameter(AppsFlyerConstants.DEEP_LINK_VALUE, deepLinkValue)
 
-        channel?.let {
-            generator.setChannel(it)
-        }
-        referrerCustomerId?.let {
-            generator.setReferrerCustomerId(it)
-        }
-        referrerName?.let {
-            generator.setReferrerName(it)
-        }
-        referrerUID?.let {
-            generator.setReferrerUID(it)
-        }
-        campaign?.let {
-            generator.setCampaign(it)
-        }
-        baseDeepLink?.let {
-            generator.setBaseDeeplink(it)
-        }
-        deepLinkPath?.let {
-            generator.setDeeplinkPath(it)
-        }
-        referrerImageURL?.let {
-            generator.setReferrerImageURL(it)
-        }
+            channel?.let {
+                generator.setChannel(it)
+            }
+            referrerCustomerId?.let {
+                generator.setReferrerCustomerId(it)
+            }
+            referrerName?.let {
+                generator.setReferrerName(it)
+            }
+            referrerUID?.let {
+                generator.setReferrerUID(it)
+            }
+            campaign?.let {
+                generator.setCampaign(it)
+            }
+            baseDeepLink?.let {
+                generator.setBaseDeeplink(it)
+            }
+            deepLinkPath?.let {
+                generator.setDeeplinkPath(it)
+            }
+            referrerImageURL?.let {
+                generator.setReferrerImageURL(it)
+            }
 
-        baseURL?.let {
-            generator.setBaseURL(it.oneLinkID, it.domain, it.appPackage)
-        }
+            baseURL?.let {
+                generator.setBaseURL(it.oneLinkID, it.domain, it.appPackage)
+            }
 
-        parameters?.let {
-            generator.addParameters(it)
-        }
+            parameters?.let {
+                generator.addParameters(it)
+            }
 
-        brandDomain?.let {
-            generator.setBrandDomain(it)
-        }
-        baseURL?.let {
-            generator.setBaseURL(it.oneLinkID, it.domain, it.appPackage)
-        }
-        parameters?.let {
-            generator.addParameters(it)
-        }
-        customShortLink?.let {
-            generator.addParameter("af_custom_shortlink", customShortLink)
-        }
+            brandDomain?.let {
+                generator.setBrandDomain(it)
+            }
+            baseURL?.let {
+                generator.setBaseURL(it.oneLinkID, it.domain, it.appPackage)
+            }
+            parameters?.let {
+                generator.addParameters(it)
+            }
+            customShortLink?.let {
+                generator.addParameter("af_custom_shortlink", customShortLink)
+            }
 
-        return generator.generateLink()
+            generator.generateLink(context, object : LinkGenerator.ResponseListener {
+                override fun onResponse(p0: String?) {
+                    cont.tryResume(p0)
+                }
+
+                override fun onResponseError(p0: String?) {
+                    cont.tryResumeWithException(
+                        IllegalStateException("Error generating share link: $p0")
+                    )
+                }
+            })
+        }
     }
 }
